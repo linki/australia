28019f344f24b2486362d37a400dc01a2eca0361
Martin Linkhorst <martin.linkhorst@student.hpi.uni-potsdam.de>
2009-09-07 23:38:50 +1000

    nice images

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   has_attached_file :image,
-                    :styles => { :medium => "300x300>", :thumb => "100x100>" }
+                    :styles => { :large => "1024x1024>", :medium => "300x300>", :thumb => "100x100>" }
 #                    :storage => :s3,
 #                    :s3_credentials => "#{RAILS_ROOT}/config/amazon_s3.yml",
 #                    :path => ":attachment/:id/:style.:extension"
