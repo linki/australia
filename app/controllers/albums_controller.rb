3d94eafd5199851f9820f28326d9b31b6f12a8a3
Martin Linkhorst <martin.linkhorst@student.hpi.uni-potsdam.de>
2009-09-22 20:07:24 +1000

    ordering of albums changed

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,7 +2,7 @@ class AlbumsController < InheritedResources::Base
   respond_to :html, :xml, :json, :js
   
   def index
-    @albums = Album.all(:order => 'created_at DESC')
+    @albums = Album.all(:order => 'starts_at DESC, ends_at DESC')
     index!
   end
   
