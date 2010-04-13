b0c70deb8c9f85191397c78ad50b52f32abdbbe0
Linki <m.linkhorst@googlemail.com>
2010-04-13 22:58:33 +0200

    readded config.gem command to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,24 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem "publishable"
+  config.gem "aws-s3", :lib => "aws/s3"
+
+  config.gem "formtastic"
+  config.gem "inherited_resources", :version => '<= 1.0.6'
+  config.gem 'ryanb-acts-as-list', :lib => 'acts_as_list', :source => 'http://gems.github.com'
+  config.gem "acl9"
+
+  config.gem 'delayed_job'
+
+  config.gem 'mime-types', :lib => "mime/types"
+
+  config.gem "haml"
+  config.gem "rubyzip", :lib => 'zip/zip'
+
+  config.gem 'bullet'
+
   config.load_paths << File.expand_path(File.join(Rails.root, 'app', 'middleware'))
+
   config.time_zone = 'Brisbane'
 end
